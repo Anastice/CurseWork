44cefedea5172e31731047c19e29e2e43b32774d
Anastice <forwork431@gmail.com>
2021-06-04 11:57:40 +0300

    musicom-deploy

---
+++ musicom-registration/musicom-registration/src/main/java/com/example/musicom/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.musicom.domain;
+
+public enum Role {
+    USER;
+}
---
+++ musicom-registration/musicom-registration/src/main/java/com/example/musicom/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.musicom.repos;
+
+import com.example.musicom.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
